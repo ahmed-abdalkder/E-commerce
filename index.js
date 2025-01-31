4b3aceeb6ef7b6a99547c75a63b40967900850e0
Ahmed Abdulkadir <aabdelkder20@gmail.com>
2025-01-30 18:30:23 -0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
- 
+import path from 'path'
  import dotenv from 'dotenv'
- dotenv.config()
+ dotenv.config({path:path.resolve('config/.env')})
  import express from'express'
 import { initApp } from './init.APP.js'
 
